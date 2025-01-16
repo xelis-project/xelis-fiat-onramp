07ced697d702e6e457a8903421f34735ccb041fc
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-01-16 15:16:41 -0500

    add npx prefix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ npm i
 # vendors
 # this is an example on how to add a npm package as vendors for client side
 # esbuild ./node_modules/query-string/index.js --bundle --format=iife --outfile=./public/vendors/querystring.js --global-name=querystring
-esbuild ./node_modules/flag-icons/css/flag-icons.css --bundle --outdir=./public/vendors/flag-icons/ --loader:.svg=file
+npx esbuild ./node_modules/flag-icons/css/flag-icons.css --bundle --outdir=./public/vendors/flag-icons/ --loader:.svg=file
 
 # add browser prefixes to stylesheets
 npx postcss ./public/styles/*.css --use autoprefixer --replace --no-map
