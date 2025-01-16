ad6ad622d2caf2e1d5bbbe53542cb5af8da5b078
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-01-16 14:19:08 -0500

    delete redirect link endpoint (use client side instead)

--- functions/redirect_link.ts
+++
@@ -1,9 +0,0 @@
-import { Ctx } from "../global";
-
-export async function onRequest(ctx: Ctx) {
-  
-
-  return new Response(JSON.stringify(""), {
-    headers: { "Content-Type": "application/json" }
-  })
-}
